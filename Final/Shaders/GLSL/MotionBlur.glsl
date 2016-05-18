e08cc46cfc3671b197046b75a7907d435c65c164
1vanK <1vanK@users.noreply.github.com>
2016-05-18 20:00:36 +0300

    Optimization

--- Final/Shaders/GLSL/MotionBlur.glsl
+++ Final/Shaders/GLSL/MotionBlur.glsl
@@ -37,19 +37,24 @@ void PS()
     vec2 oldScreenPos = vec2(oldClipPos.x * vGBufferOffsets.z + vGBufferOffsets.x * oldClipPos.w,
                         oldClipPos.y * vGBufferOffsets.w + vGBufferOffsets.y * oldClipPos.w);
 
-    vec2 delta = (vScreenPos.xy - oldScreenPos);
-    delta = delta / cTimeStep / 20.0;
+    vec2 dist = (vScreenPos.xy - oldScreenPos);
+    dist = dist / cTimeStep / 20.0;
 
-    vec3 sum = vec3(0.0);
-    float samples = 20;
+    int samples = 20;
     
-    for (float i = 0.0; i < samples; i++)
+    // Суммируем пиксели в направлении движения на расстояние dist / 2 в каждую сторону
+    // от рассматриваемого пикселя.
+    vec2 pos = vScreenPos.xy + dist * -0.5;
+    vec2 step = dist / (samples - 1);
+    vec3 sum = texture2D(sDiffMap, pos).rgb;
+    
+    for (int i = 1; i < samples; i++)
     {
-        vec2 pos = vScreenPos.xy + delta * ( (i / (samples - 1)) - 0.5 );
+        pos += step;
         sum += texture2D(sDiffMap, pos).rgb;
-	}
+    }
 
-	vec3 color = sum / samples;
+    vec3 color = sum / samples;
 	
     gl_FragColor = vec4(color, 1);
 }
