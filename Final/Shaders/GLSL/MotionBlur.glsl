1d218455ab2b73ccff6ae4c11e00bba9cae52fb7
1vanK <1vanK@users.noreply.github.com>
2016-05-18 20:40:20 +0300

    Update

--- Final/Shaders/GLSL/MotionBlur.glsl
+++ Final/Shaders/GLSL/MotionBlur.glsl
@@ -37,15 +37,26 @@ void PS()
     vec2 oldScreenPos = vec2(oldClipPos.x * vGBufferOffsets.z + vGBufferOffsets.x * oldClipPos.w,
                         oldClipPos.y * vGBufferOffsets.w + vGBufferOffsets.y * oldClipPos.w);
 
-    vec2 dist = (vScreenPos.xy - oldScreenPos);
-    dist = dist / cTimeStep / 20.0;
+    // Расстояние на экране, которое прошел пиксель с прошлого кадра.
+    vec2 offset = (vScreenPos.xy - oldScreenPos);
+    // При большой частоте кадров расстояния очень малы и эффект размытия становится незаметен.
+    // Поэтому используем промежуток времени с прошлого кадра как коэффициент, чтобы сделать эффект
+    // размытия независимым от ФПС.
+    // В данном случае эффект рассчитывается как для ФПС = 20 кадров в секунду.
+    offset = offset / (cTimeStep * 20.0);
+
+    // ФПС = число кадров / время.
+    // Так как нам нужен коэффициент, приводящий текущий ФПС к 20 кадрам в в секунду, то
+    // текущий ФПС = 20 * некий коэффициент.
+    // Значит некий коэффициент = текущий фпс / 20. Текущий ФПС = также равен 1 / время.
+    // Значит коэффициент = 1 / (время * 20).
 
     int samples = 20;
     
     // Суммируем пиксели в направлении движения на расстояние dist / 2 в каждую сторону
     // от рассматриваемого пикселя.
-    vec2 pos = vScreenPos.xy + dist * -0.5;
-    vec2 step = dist / (samples - 1);
+    vec2 pos = vScreenPos.xy + offset * -0.5;
+    vec2 step = offset / (samples - 1);
     vec3 sum = texture2D(sDiffMap, pos).rgb;
     
     for (int i = 1; i < samples; i++)
